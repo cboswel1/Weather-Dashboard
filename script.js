c70e334828d397441f46b3d200039b65d89b0f57
cboswel1 <christianboswell86@gmail.com>
2020-08-24 19:00:25 -0600

    current city append

--- script.js
+++ script.js
@@ -14,31 +14,56 @@
 
 //API call: https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=9f0120827a50e9a11f1c94d939f4dbfc
 
-//var for city search value to insert into api call 
-var citySearch = $(".city-search").val();
+
 
 //API Calls for current weather and 5 day forecast 
-var currentW = "api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=9f0120827a50e9a11f1c94d939f4dbfc";
-console.log(currentW);
 
-var fiveDayW = "api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=9f0120827a50e9a11f1c94d939f4dbfc"; 
-console.log(fiveDayW);
 
 
 //Creating Search button variable 
 var searchButton = $(".search-button");
 console.log("Working");
 
+
+
+
 //When user clicks search...event listener for click 
 searchButton.click(function() {
-
 //current weather conditions 
+//var for city search value to insert into api call 
+var citySearch = $(".citySearch").val();
+console.log(citySearch);
 
-    $ajax({
+var currentW = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=9f0120827a50e9a11f1c94d939f4dbfc&units=imperial";
+console.log(currentW);
+
+var fiveDayW = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=9f0120827a50e9a11f1c94d939f4dbfc&units=imperial"; 
+console.log(fiveDayW);
+
+    $.ajax({
         url: currentW, 
         method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
+        console.log(currentW);
 
+        console.log(response);
+
+       
+
+        var cWeather = $("#current-weather"); 
+
+        var currentCity = $("<p>").text(response.name + response.dt + response.weather.icon);
+        console.log(currentCity);
+        var currentTemp = $("<p>").text("Current Temperature: " + response.main.temp);
+        console.log(currentTemp);
+        var currentHum = $("<p>").text("Current Humidity: " + response.main.humidity);
+        console.log(currentHum);
+        var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed);
+
+        // var uvIndex = $("<td>").text(response.)
+        cWeather.append(currentCity, currentTemp, currentHum, windSpeed); 
+        // $("#current-weather").append(cWeather);
+        
 
     });
     
