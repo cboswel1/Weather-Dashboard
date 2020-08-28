3db2659bde93823a0b79240df68deff6f3f722d5
cboswel1 <christianboswell86@gmail.com>
2020-08-28 16:43:23 -0600

    js refactor

--- script.js
+++ script.js
@@ -138,120 +138,44 @@ searchButton.click(function() {
     }).then(function (response) {
 
 
-        //Day 1
+        //Day 1 - Worked with TA to re-factor for less code. Original was repeating each day. 
+
         cardFun(1, i, "one");
-        // // Future Date
-        // var dayOneDate = moment().add(1, 'day').format('dddd');
-        // var dayOne = $(".day-one-date").text(dayOneDate);
+        function cardFun(addNumber, apiList, currentCDay) {
+            
+            //future date
+            var dayOneDate = moment().add(addNumber, 'day').format('dddd');
+            var dayOne = $(".day-" + currentCDay + "-date").text(dayOneDate);
 
-        // //Icon for Future Date
-        // var dayOneIcons = response.list[i].weather[0].icon;
-        // var weatherIcon = "https://openweathermap.org/img/wn/" + dayOneIcons + ".png"
-        // var oneIcon = $(".day-one-icon").attr('src', weatherIcon);
+            //icon for future date
+            var dayFiveIcons = response.list[apiList].weather[0].icon;
+            var weatherIcon = "https://openweathermap.org/img/wn/" + dayFiveIcons + ".png"
+            var oneIcon = $(".day-" + currentCDay + "-icon").attr('src', weatherIcon);
+         
+            //creating var and clearing for append 
+            var fiveDay1 = $(".day-" + currentCDay); 
+            fiveDay1.empty();
 
-        // //Creating var and clearing for append
-        // var fiveDay1 = $(".day-one"); 
-        // fiveDay1.empty();
-
-        // //setting temp / hum to var
-        // var dayOneTemp = $("<p>").text("Temperature: " + response.list[0].main.temp + " °F");
-        // var dayOneHum = $("<p>").text("Humidity: " + response.list[0].main.humidity + "%");
-
-        // //appending to 5 day
-        // fiveDay1.append(dayOneTemp, dayOneHum); 
-
-
+            //setting temp/hum for var
+            var dayOneTemp = $("<p>").text("Temperature: " + response.list[apiList].main.temp + " °F");
+            var dayOneHum = $("<p>").text("Humidity: " + response.list[apiList].main.humidity + "%");
+        
+            //append 5 day 
+            fiveDay1.append(dayOneTemp, dayOneHum); 
+        }
+        
         //Day 2
         cardFun(2, 8, "two");
-        // var dayTwoDate = moment().add(2, 'day').format('dddd');
-        // var dayTwo = $(".day-two-date").text(dayTwoDate);
-
-        // var dayTwoIcons = response.list[8].weather[0].icon;
-        // var weatherIcon2 = "https://openweathermap.org/img/wn/" + dayTwoIcons + ".png"
-        // var twoIcon = $(".day-two-icon").attr('src', weatherIcon2);
-
-        // var fiveDay2 = $(".day-two"); 
-        // fiveDay2.empty();
-
-        // var dayTwoTemp = $("<p>").text("Temperature: " + response.list[8].main.temp + " °F");
-        // var dayTwoHum = $("<p>").text("Humidity: " + response.list[8].main.humidity + "%");
-        
-        // fiveDay2.append(dayTwoTemp, dayTwoHum); 
-
 
         //Day 3
         cardFun(3, 16, "three");
-        // var dayThreeDate = moment().add(3, 'day').format('dddd');
-        // var dayThree = $(".day-three-date").text(dayThreeDate);
-
-        // var dayThreeIcons = response.list[16].weather[0].icon;
-        // var weatherIcon3 = "https://openweathermap.org/img/wn/" + dayThreeIcons + ".png"
-        // var threeIcon = $(".day-three-icon").attr('src', weatherIcon3);
-
-        // var fiveDay3 = $(".day-three"); 
-        // fiveDay3.empty();
-
-        // var dayThreeTemp = $("<p>").text("Temperature: " + response.list[16].main.temp + " °F");
-        // var dayThreeHum = $("<p>").text("Humidity: " + response.list[16].main.humidity + "%");
-    
-        // fiveDay3.append(dayThreeTemp, dayThreeHum); 
-
 
         //Day 4
         cardFun(4, 24, "four");
-        // var dayFourDate = moment().add(4, 'day').format('dddd');
-        // var dayFour = $(".day-four-date").text(dayFourDate);
-
-        // var dayFourIcons = response.list[24].weather[0].icon;
-        // var weatherIcon4 = "https://openweathermap.org/img/wn/" + dayFourIcons + ".png"
-        // var fourIcon = $(".day-four-icon").attr('src', weatherIcon4);
         
-        // var fiveDay4 = $(".day-four"); 
-        // fiveDay4.empty();
-
-        // var dayFourTemp = $("<p>").text("Temperature: " + response.list[24].main.temp + " °F");
-        // var dayFourHum = $("<p>").text("Humidity: " + response.list[24].main.humidity + "%");
-        
-        // fiveDay4.append(dayFourTemp, dayFourHum); 
-
-
         //Day 5
-
-        // var dayFiveDate = moment().add(5, 'day').format('dddd');
-        // var dayFive = $(".day-five-date").text(dayFiveDate);
-
-        // var dayFiveIcons = response.list[32].weather[0].icon;
-        // var weatherIcon5 = "https://openweathermap.org/img/wn/" + dayFiveIcons + ".png"
-        // var fiveIcon = $(".day-five-icon").attr('src', weatherIcon5);
-         
-        // var fiveDay5 = $(".day-five"); 
-        // fiveDay5.empty();
-
-        // var dayFiveTemp = $("<p>").text("Temperature: " + response.list[32].main.temp + " °F");
-        // var dayFiveHum = $("<p>").text("Humidity: " + response.list[32].main.humidity + "%");
-        
-        // fiveDay5.append(dayFiveTemp, dayFiveHum); 
-
         cardFun(5, 32, "five");
-        function cardFun(addNumber, apiList, currentCDay) {
-            // day add number 
-            // api list index 
-            // current class day 
-            var dayFiveDate = moment().add(addNumber, 'day').format('dddd');
-            var dayFive = $(".day-" + currentCDay + "-date").text(dayFiveDate);
-
-            var dayFiveIcons = response.list[apiList].weather[0].icon;
-            var weatherIcon5 = "https://openweathermap.org/img/wn/" + dayFiveIcons + ".png"
-            var fiveIcon = $(".day-" + currentCDay + "-icon").attr('src', weatherIcon5);
-         
-            var fiveDay5 = $(".day-" + currentCDay); 
-            fiveDay5.empty();
-
-            var dayFiveTemp = $("<p>").text("Temperature: " + response.list[apiList].main.temp + " °F");
-            var dayFiveHum = $("<p>").text("Humidity: " + response.list[apiList].main.humidity + "%");
         
-            fiveDay5.append(dayFiveTemp, dayFiveHum); 
-        }
     });
 }); 
 
